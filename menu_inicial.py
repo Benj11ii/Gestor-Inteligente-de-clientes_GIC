1fd0f85704441548c3b6c60a863b04705c815d71
Benj11ii <benjaminalonsocarmona@gmail.com>
2026-02-19 15:44:42 -0300

    Libreria actualizada

--- menu_inicial.py
+++ menu_inicial.py
@@ -3,7 +3,7 @@ import os
 from modelos import ClienteRegular, ClientePremium, ClienteCorporativo
 from gestor_archivos import guardar_clientes, cargar_clientes
 from conexion_api_externa import validar_formato_id, enviar_email_bienvenida
-from conexion_api_externa import validar_email_con_api, validar_formato_local
+from conexion_api_externa import validar_email_con_api
 
 ## Muestra las opciones del menu
 def mostrar_menu():
